fb5aa3ecbbe35dd8f267bb4142792e8738c78be0
Alexander Pope <alex@pope-industries.com>
2016-11-03 15:41:55 +0100

    checkpoint

--- src/index.js
+++ src/index.js
@@ -7,7 +7,6 @@
 
 'use strict';
 
-const agent = require('@yr/agent');
 const assign = require('object-assign');
 const clock = require('@yr/clock');
 const Cursor = require('./lib/cursor');
@@ -17,37 +16,21 @@ const isPlainObject = require('is-plain-obj');
 const keys = require('@yr/keys');
 const property = require('@yr/property');
 const runtime = require('@yr/runtime');
-const time = require('@yr/time');
-const uuid = require('uuid');
 
-const DEFAULT_LATENCY = 10000;
-const DEFAULT_LOAD_OPTIONS = {
-  expiry: 60000,
-  retry: 2,
-  timeout: 5000
-};
 const DEFAULT_SET_OPTIONS = {
   // Browser immutable by default
   immutable: runtime.isBrowser,
-  reload: false,
   serialisable: true,
   merge: true
 };
-const DEFAULT_STORAGE_OPTIONS = {
-  keyLength: 2
-};
-const DELEGATED_METHODS = [
-  'fetch',
+const DEFAULT_HANDLED_METHODS = [
   'get',
-  'load',
-  'persist',
-  'reload',
   'set',
-  'unpersist',
   'unset',
-  'update',
-  'upgradeStorageData'
+  'update'
 ];
+
+let uid = 0;
 
 /**
  * Instance factory
@@ -66,15 +49,8 @@ class DataStore extends Emitter {
    * @param {String} [id]
    * @param {Object} [data]
    * @param {Object} [options]
-   *  - {Object} handlers method:key
-   *  - {Object} loading
-   *    - {Number} expiry
-   *    - {Number} retry
-   *    - {Number} timeout
-   *  - {Object} serialisable key:Boolean
-   *  - {Object} storage
-   *    - {Number} keyLength
-   *    - {Object} store
+   *  - {Object} handlers
+   *  - {Object} serialisable
    *  - {Boolean} writable
    */
   constructor (id, data, options = {}) {
@@ -82,63 +58,113 @@ class DataStore extends Emitter {
 
     this.debug = Debug('yr:data' + (id ? ':' + id : ''));
     this.destroyed = false;
-    this.uid = uuid.v4();
-    this.id = id || `store${--this.uid}`;
+    this.id = id || `store${++uid}`;
     this.writable = 'writable' in options ? options.writable : true;
 
     this._cursors = {};
     this._data = {};
     this._handlers = {};
-    this._loading = assign({}, DEFAULT_LOAD_OPTIONS, options.loading);
     this._serialisable = options.serialisable || {};
-    this._storage = assign({}, DEFAULT_STORAGE_OPTIONS, options.storage);
 
-    // Generate delegated methods
-    for (const method of DELEGATED_METHODS) {
-      const privateMethod = `_${method}`;
+    DEFAULT_HANDLED_METHODS.forEach(this.registerHandledMethod, this);
+    if (options.handlers) this.registerHandlers(options.handlers);
 
-      this[method] = this._route.bind(this, privateMethod);
-      this[privateMethod] = this[privateMethod].bind(this);
-      // Setup handlers
-      this._handlers[privateMethod] = {};
-      if (options.handlers && method in options.handlers) {
-        for (const namespace in options.handlers[method]) {
-          this.registerHandler(method, namespace, options.handlers[method][namespace]);
-        }
-      }
-    }
-
-    this.bootstrap(this._storage, data || {});
+    this.bootstrap(data || {});
   }
 
   /**
-   * Bootstrap from 'storage' and/or 'data'
-   * @param {Object} storage
+   * Register 'fn' as 'methodName'
+   * @param {String} methodName
+   * @param {Function} [fn]
+   */
+  registerHandledMethod (methodName, fn) {
+    const privateMethodName = `_${methodName}`;
+
+    fn = fn || this[privateMethodName];
+
+    if (!fn) return;
+
+    this[methodName] = this._route.bind(this, privateMethodName);
+    this[privateMethodName] = fn.bind(this);
+  }
+
+  /**
+   * Register 'handler' for 'methodName' and 'namespace'
+   * @param {String} methodName
+   * @param {String} namespace
+   * @param {Function} handler
+   */
+  registerHandler (methodName, namespace, handler) {
+    const privateMethodName = `_${methodName}`;
+
+    if (!this._handlers[privateMethodName]) this._handlers[privateMethodName] = [];
+
+    this._handlers[privateMethodName].push({ handler, namespace });
+
+    // const scopedMethod = (key, ...args) => this[privateMethodName](keys.join(namespace, key), ...args);
+
+    // this._handlers[privateMethodName][namespace] = { handler, scopedMethod };
+  }
+
+  /**
+   * Route 'privateMethodName' to appropriate handler
+   * depending on passed 'key' (args[0])
+   * @param {String} privateMethodName
+   * @param {*} args
+   * @returns {Object|null}
+   */
+  _route (privateMethodName, ...args) {
+    let [key = '', ...rest] = args;
+
+    if (!key) return this[privateMethodName](...args);
+
+    if ('string' == typeof key) {
+      if (key.charAt(0) == '/') key = key.slice(1);
+
+      if (this._handlers[privateMethodName].length) {
+        const handlers = this._handlers[privateMethodName].filter(({ handler, namespace }) => {
+          // Will match if handler.namespace == ''
+          return key.indexOf(namespace) == 0;
+        });
+
+        handlers.reduce((sequence, { handler, namespace }) => {
+          return sequence.then(() => handler());
+        }, Promise.resolve([]));
+      }
+
+      const namespace = keys.first(key);
+
+      // Route to handler if it exists
+      if (namespace && namespace in this._handlers[privateMethodName]) {
+        const { handler, scopedMethod } = this._handlers[privateMethodName][namespace];
+
+        return handler(this, scopedMethod, keys.slice(key, 1), ...rest);
+      }
+      return this[privateMethodName](key, ...rest);
+    }
+
+    // Batch (set, update, load, etc)
+    if (isPlainObject(key)) {
+      for (const k in key) {
+        this._route(privateMethodName, k, key[k], ...rest);
+      }
+      return;
+    }
+
+    // Array of keys (get)
+    if (Array.isArray(key)) {
+      return key.map((k) => {
+        return this._route(privateMethodName, k, ...rest);
+      });
+    }
+  }
+
+  /**
+   * Bootstrap from 'data'
    * @param {Object} data
    */
-  bootstrap (storage, data) {
-    const { keyLength, store } = storage;
+  bootstrap (data) {
     const options = { immutable: false };
-
-    if (store) {
-      let storageData = property.reshape(store.get('/'), 1);
-
-      for (const namespace in storageData) {
-        const value = storageData[namespace];
-
-        // Handle version mismatch
-        if (store.shouldUpgrade(namespace)) {
-          // Clear all storage data for namespace
-          for (const key in property.reshape(value, keyLength - 1)) {
-            store.remove(keys.join(namespace, key));
-          }
-          // Allow handlers to override
-          storageData[namespace] = this.upgradeStorageData(namespace, value);
-        }
-      }
-      // TODO: persist
-      this.set(storageData, options);
-    }
 
     this.set(data, options);
   }
@@ -163,86 +189,6 @@ class DataStore extends Emitter {
   }
 
   /**
-   * Retrieve storage keys for 'key'
-   * based on storage.keyLength
-   * @param {String} key
-   * @returns {Array}
-   */
-  getStorageKeys (key = '') {
-    const { keyLength } = this._storage;
-    const length = keys.length(key);
-
-    if (length < keyLength) {
-      const parentData = property.reshape(this._get(keys.slice(key, 0, -1)), keyLength);
-
-      return Object.keys(parentData)
-        .filter((k) => k.indexOf(key) == 0)
-        .map((k) => `/${k}`);
-    }
-
-    return [`/${keys.slice(key, 0, this._storage.keyLength)}`];
-  }
-
-  /**
-   * Register 'handler' for 'method' and 'namespace'
-   * @param {String} method
-   * @param {String} namespace
-   * @param {Function} handler
-   */
-  registerHandler (method, namespace, handler) {
-    const privateMethod = `_${method}`;
-
-    // Prevent overwriting
-    if (!this._handlers[privateMethod][namespace]) {
-      const scopedMethod = (key, ...args) => this[privateMethod](keys.join(namespace, key), ...args);
-
-      this._handlers[privateMethod][namespace] = { handler, scopedMethod };
-    }
-  }
-
-  /**
-   * Route 'method' to appropriate handler
-   * depending on passed 'key' (args[0])
-   * @param {String} method
-   * @param {*} args
-   * @returns {Object|null}
-   */
-  _route (method, ...args) {
-    let [key = '', ...rest] = args;
-
-    if (!key) return this[method](...args);
-
-    if ('string' == typeof key) {
-      if (key.charAt(0) == '/') key = key.slice(1);
-
-      const namespace = keys.first(key);
-
-      // Route to handler if it exists
-      if (namespace && namespace in this._handlers[method]) {
-        const { handler, scopedMethod } = this._handlers[method][namespace];
-
-        return handler(this, scopedMethod, keys.slice(key, 1), ...rest);
-      }
-      return this[method](key, ...rest);
-    }
-
-    // Batch (set, update, load, etc)
-    if (isPlainObject(key)) {
-      for (const k in key) {
-        this._route(method, k, key[k], ...rest);
-      }
-      return;
-    }
-
-    // Array of keys (get)
-    if (Array.isArray(key)) {
-      return key.map((k) => {
-        return this._route(method, k, ...rest);
-      });
-    }
-  }
-
-  /**
    * Retrieve property value with `key`
    * @param {String} [key]
    * @returns {Object}
@@ -254,11 +200,11 @@ class DataStore extends Emitter {
     const value = property.get(this._data, key);
 
     // Check expiry
-    if (Array.isArray(value)) {
-      value.forEach(checkExpiry);
-    } else {
-      checkExpiry(value);
-    }
+    // if (Array.isArray(value)) {
+    //   value.forEach(checkExpiry);
+    // } else {
+    //   checkExpiry(value);
+    // }
 
     return value;
   }
@@ -271,7 +217,7 @@ class DataStore extends Emitter {
    *  - {Boolean} immutable
    *  - {Boolean} reference
    *  - {Boolean} merge
-   * @returns {Object}
+   * @returns {null}
    */
   _set (key, value, options) {
     if (this.writable) {
@@ -305,8 +251,6 @@ class DataStore extends Emitter {
       // Handle persistence
       if ('persistent' in options && options.persistent) this._persist(key);
     }
-
-    return value;
   }
 
   /**
@@ -371,217 +315,6 @@ class DataStore extends Emitter {
   }
 
   /**
-   * Load data from 'url' and store at 'key'
-   * @param {String} key
-   * @param {String} url
-   * @param {Object} [options]
-   *  - {Boolean} abort
-   *  - {Boolean} ignoreQuery
-   * @returns {Response}
-   */
-  _load (key, url, options) {
-    options = options || {};
-    options.id = this.uid;
-
-    this.debug('load %s from %s', key, url);
-
-    return agent
-      .get(url, options)
-      .timeout(this._loading.timeout)
-      .retry(this._loading.retry)
-      .then((res) => {
-        this.debug('loaded "%s" in %dms', key, res.duration);
-
-        let value;
-
-        // Guard against empty data
-        if (res.body) {
-          // TODO: make more generic with bodyParser option/handler
-          // Handle locations results separately
-          let data = ('totalResults' in res.body)
-            ? (res.body._embedded && res.body._embedded.location) || []
-            : res.body;
-
-          // Add expires header
-          if (res.headers && 'expires' in res.headers) {
-            const expires = getExpiry(res.headers.expires, this._loading.expiry);
-
-            if (Array.isArray(data)) {
-              data.forEach((d) => {
-                if (isPlainObject(d)) {
-                  d.expires = expires;
-                  d.expired = false;
-                }
-              });
-            } else {
-              data.expires = expires;
-              data.expired = false;
-            }
-          }
-
-          // Guard against parse errors during set()
-          try {
-            // Merge with existing
-            options.merge = true;
-            // All remote data stored with reference key
-            options.reference = true;
-            value = this.set(key, data, options);
-          } catch (err) {
-            this.debug('failed to store remote resource "%s" from %s', key, url);
-            // TODO: update error message?
-            err.status = 500;
-            throw err;
-          }
-        }
-
-        this.emit('load:' + key, value);
-        this.emit('load', key, value);
-
-        return res;
-      })
-      .catch((err) => {
-        this.debug('unable to load "%s" from %s', key, url);
-
-        // Remove if not found or malformed (but not aborted)
-        if (err.status < 499) this.remove(key);
-
-        throw err;
-      });
-  }
-
-  /**
-   * Reload data from 'url'
-   * @param {String} key
-   * @param {String} url
-   * @param {Object} [options]
-   *  - {Boolean} abort
-   *  - {Boolean} ignoreQuery
-   *  - {Boolean} reload
-   */
-  _reload (key, url, options) {
-    options = options || {};
-    if (!options.reload) return;
-
-    const reload = () => {
-      this._load(key, url, options)
-        .then((res) => {
-          const value = this.get(key);
-
-          this.emit('reload:' + key, value);
-          this.emit('reload', key, value);
-          this._reload(key, url, options);
-        })
-        .catch((err) => {
-          // TODO: error never logged
-          this.debug('unable to reload "%s" from %s', key, url);
-          this._reload(key, url, options);
-        });
-    };
-    const value = this.get(key);
-    // Guard against invalid duration
-    const duration = Math.max((value && value.expires || 0) - Date.now(), this._loading.expiry);
-
-    this.debug('reloading "%s" in %dms', key, duration);
-    // Set custom id
-    clock.timeout(duration, reload, url);
-  }
-
-  /**
-   * Fetch data. If expired, load from 'url' and store at 'key'
-   * @param {String} key
-   * @param {String} url
-   * @param {Object} [options]
-   *  - {Boolean} abort
-   *  - {Boolean} ignoreQuery
-   *  - {Boolean} reload
-   *  - {Boolean} staleWhileRevalidate
-   *  - {Boolean} staleWhileError
-   * @returns {Promise}
-   */
-  _fetch (key, url, options) {
-    options = options || {};
-
-    this.debug('fetch %s from %s', key, url);
-
-    // Set expired state
-    const value = this.get(key);
-
-    // Load if not found or expired
-    if (!value || value.expired) {
-      const load = new Promise((resolve, reject) => {
-        this._load(key, url, options)
-          .then((res) => {
-            // Schedule a reload
-            this._reload(key, url, options);
-            resolve({
-              duration: res.duration,
-              headers: res.headers,
-              data: this.get(key)
-            });
-          })
-          .catch((err) => {
-            // Schedule a reload if error
-            if (err.status >= 500) this._reload(key, url, options);
-            resolve({
-              duration: 0,
-              error: err,
-              headers: { status: err.status },
-              data: options.staleWhileError ? value : null
-            });
-          });
-      });
-
-      // Wait for load unless stale and staleWhileRevalidate
-      if (!(value && options.staleWhileRevalidate)) return load;
-    }
-
-    // Schedule a reload
-    this._reload(key, url, options);
-    // Return data (possibly stale)
-    return Promise.resolve({
-      duration: 0,
-      headers: { status: 200 },
-      data: value
-    });
-  }
-
-  /**
-   * Save to local storage
-   * @param {String} key
-   */
-  _persist (key) {
-    if (this._storage.store) {
-      this.getStorageKeys(key).forEach((storageKey) => {
-        // Storage keys are global, so trim
-        this._storage.store.set(storageKey, this._get(storageKey.slice(1)));
-      });
-    }
-  }
-
-  /**
-   * Remove from local storage
-   * @param {String} key
-   */
-  _unpersist (key) {
-    if (this._storage.store) {
-      this.getStorageKeys(key).forEach((storageKey) => {
-        this._storage.store.remove(storageKey);
-      });
-    }
-  }
-
-  /**
-   * Update storage when versions don't match
-   * @param {String} key
-   * @param {Object} value
-   * @returns {Object}
-   */
-  _upgradeStorageData (key, value) {
-    // Delete as default
-    return null;
-  }
-
-  /**
    * Retrieve an instance reference at 'key' to a subset of data
    * @param {String} key
    * @returns {DataStore}
@@ -619,19 +352,10 @@ class DataStore extends Emitter {
   }
 
   /**
-   * Abort all outstanding load/reload requests
-   */
-  abort () {
-    // TODO: return aborted urls and use in clock.cancel
-    agent.abortAll(this.uid);
-    // clock.cancelAll(this.id);
-  }
-
-  /**
    * Destroy instance
    */
   destroy () {
-    this.abort();
+    // this.abort();
 
     // Destroy cursors
     for (const key in this._cursors) {
@@ -700,7 +424,7 @@ class DataStore extends Emitter {
         if (this._serialisable[keyChain] !== false) {
           if (isPlainObject(data[prop])) {
             obj[prop] = this._serialise(keyChain, data[prop]);
-          } else if (time.isTime(data[prop])) {
+          } else if ('object' == typeof data[prop] && 'toJSON' in data[prop]) {
             obj[prop] = data[prop].toJSON();
           } else {
             obj[prop] = data[prop];
@@ -714,33 +438,5 @@ class DataStore extends Emitter {
     return (this._serialisable[key] !== false)
       ? data
       : null;
-  }
-}
-
-/**
- * Retrieve expiry from 'dateString'
- * @param {Number} dateString
- * @param {Number} minimum
- * @returns {Number}
- */
-function getExpiry (dateString, minimum) {
-  // Add latency overhead to compensate for transmission time
-  const expires = +(new Date(dateString)) + DEFAULT_LATENCY;
-  const now = Date.now();
-
-  return (expires > now)
-    ? expires
-    // Local clock is set incorrectly
-    : now + minimum;
-}
-
-/**
- * Check if 'value' is expired
- * @param {Object} value
- */
-function checkExpiry (value) {
-  if (value && isPlainObject(value) && value.expires && Date.now() > value.expires) {
-    value.expired = true;
-    value.expires = 0;
   }
 }
