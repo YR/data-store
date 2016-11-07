7bcaf4b98893027c1893547fa6e6f1f8c1740bab
Alexander Pope <alex@pope-industries.com>
2016-11-07 15:41:39 +0100

    add memoized options/auto registration

--- src/lib/handlers/fetchWithTemplatedURL.js
+++ src/lib/handlers/fetchWithTemplatedURL.js
@@ -1,17 +1,19 @@
 'use strict';
 
+const assign = require('object-assign');
 const urlUtils = require('@yr/url-utils');
 
 /**
  * Handler factory for calling 'fetch' with templated url string
+ * @param {DataStore} store
  * @param {String} key
  * @param {String} urlTemplate
- * @param {Object} options
- * @returns {Function}
+ * @param {Object} memoizedOptions
  */
-module.exports = function handlerFactory (key, urlTemplate, options) {
-  return function fetchWithTemplatedURLHandler (store, fetch, rootKey, key, url, options) {
+module.exports = function handlerFactory (store, key, urlTemplate, memoizedOptions) {
+  store.registerHandler('fetch', key, function fetchWithTemplatedURLHandler (store, fetch, rootKey, key, url, options) {
+    options = assign({}, memoizedOptions, options);
     if ('string' != typeof url) url = urlUtils.template(urlTemplate, url);
     return fetch(rootKey, url, options);
-  };
+  });
 };
