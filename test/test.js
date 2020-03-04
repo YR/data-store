732aae9d0e82383f0fd257c4b1d1e4454cd15407
Morten Kjelling <mortenok@gmail.com>
2020-03-04 10:18:41 +0100

    Removed tests that tested functionality that was already removed

--- test/test.js
+++ test/test.js
@@ -2,7 +2,6 @@
 
 const { create: createStore } = require("../src/index");
 const { expect } = require("chai");
-const HandlerContext = require("../src/lib/HandlerContext");
 
 let store;
 
@@ -39,22 +38,6 @@ describe("DataStore", () => {
     });
   });
 
-  describe("_resolveRefKey()", () => {
-    it("should resolve key with no references", () => {
-      expect(store._resolveRefKey("bar")).to.equal("bar");
-      expect(store._resolveRefKey("zing/zoop")).to.equal("zing/zoop");
-    });
-    it("should resolve key with references", () => {
-      expect(store._resolveRefKey("foo/boo")).to.equal("boo");
-    });
-    it("should resolve nested key with references", () => {
-      expect(store._resolveRefKey("foo/boo/bat/foo")).to.equal("boo/bat/foo");
-      expect(store._resolveRefKey("foo/boo/zing/zoop")).to.equal(
-        "boo/zing/zoop"
-      );
-    });
-  });
-
   describe("get()", () => {
     it("should return a value for a string key", () => {
       expect(store.get("bar")).to.equal("bat");
@@ -63,54 +46,6 @@ describe("DataStore", () => {
     it("should return all data if no key specified", () => {
       expect(store.get().bar).to.equal("bat");
     });
-    it("should return a referenced value", () => {
-      expect(store.get("foo/boo")).to.eql({ bar: "foo", bat: { foo: "foo" } });
-      expect(store.get("foo/boo/bat/foo")).to.eql("foo");
-    });
-    it("should return a referenced value when passed a reference key", () => {
-      expect(store.get("__ref:boo")).to.eql({
-        bar: "foo",
-        bat: { foo: "foo" }
-      });
-    });
-    it('should return a referenced value when passed a reference key and "options.referenceDepth = 0"', () => {
-      expect(store.get("__ref:boo", { referenceDepth: 0 })).to.eql({
-        bar: "foo",
-        bat: { foo: "foo" }
-      });
-    });
-    it("should return a resolved object of referenced values", () => {
-      expect(store.get("foo")).to.eql({
-        bar: "foo",
-        boo: { bar: "foo", bat: { foo: "foo" } },
-        bat: "bat"
-      });
-    });
-    it('should not return a resolved object of referenced values if "options.referenceDepth = 0"', () => {
-      expect(store.get("foo", { referenceDepth: 0 })).to.eql({
-        bar: "foo",
-        boo: "__ref:boo",
-        bat: "__ref:bar"
-      });
-    });
-    it("should return a resolved array of referenced values", () => {
-      expect(store.get("boop")).to.eql([
-        "bat",
-        { bar: "foo", boo: "__ref:boo", bat: "__ref:bar" }
-      ]);
-    });
-    it('should return a deeply resolved array of referenced values when "options.referenceDepth = 2"', () => {
-      expect(store.get("boop", { referenceDepth: 2 })).to.eql([
-        "bat",
-        { bar: "foo", boo: { bar: "foo", bat: { foo: "foo" } }, bat: "bat" }
-      ]);
-    });
-    it('should not return a resolved array of referenced values if "options.referenceDepth = 0"', () => {
-      expect(store.get("boop", { referenceDepth: 0 })).to.eql([
-        "__ref:bar",
-        "__ref:foo"
-      ]);
-    });
     it("should cache read results if not writeable", () => {
       store.setWriteable(false);
       expect(store.get("boo/bat/foo")).to.equal("foo");
@@ -118,10 +53,7 @@ describe("DataStore", () => {
     });
     it('should cache read results if not writeable, respecting "options.referenceDepth"', () => {
       store.setWriteable(false);
-      expect(store.get("boop")).to.eql([
-        "bat",
-        { bar: "foo", boo: "__ref:boo", bat: "__ref:bar" }
-      ]);
+      expect(store.get("boop")).to.eql(["__ref:bar", "__ref:foo"]);
       expect(store._getCache).to.have.property("boop:1");
       expect(store.get("boop", { referenceDepth: 0 })).to.eql([
         "__ref:bar",
@@ -191,66 +123,11 @@ describe("DataStore", () => {
     });
   });
 
-  describe("reference()", () => {
-    it("should return a key reference", () => {
-      expect(store.reference("bar")).to.equal("__ref:bar");
-    });
-    it("should return an already referenced key reference", () => {
-      expect(store.reference("foo/boo")).to.equal("__ref:boo");
-    });
-  });
-
-  describe("referenceAll()", () => {
-    it("should return an array of key references", () => {
-      expect(store.referenceAll(["bar", "foo/bar"])).to.eql([
-        "__ref:bar",
-        "__ref:foo/bar"
-      ]);
-    });
-  });
-
-  describe("unreference()", () => {
-    it("should return a regular key", () => {
-      expect(store.unreference("bar")).to.equal("bar");
-    });
-    it("should return an unreferenced key", () => {
-      expect(store.unreference("__ref:bar")).to.equal("bar");
-    });
-  });
-
-  describe("unreferenceAll()", () => {
-    it("should return an array of unreferenced keys", () => {
-      expect(store.unreferenceAll(["__ref:bar", "__ref:foo/bar"])).to.eql([
-        "bar",
-        "foo/bar"
-      ]);
-    });
-  });
-
   describe("destroy()", () => {
     it("should destroy all data references", () => {
       store.destroy();
       expect(store.destroyed).to.eql(true);
       expect(store._data).to.eql({});
-    });
-  });
-
-  describe("dump()", () => {
-    it("should return a serialisable json object with no excluded properties", () => {
-      store.setSerialisabilityOfKey("bar", false);
-      const data = store.dump();
-
-      expect(data.bar).to.equal("bat");
-    });
-    it("should return an object with resolved references", () => {
-      const data = store.dump();
-
-      expect(data.foo.boo.bar).to.equal("foo");
-    });
-    it("should optionally return a serialised string", () => {
-      const json = store.dump(true);
-
-      expect(json).to.be.a("string");
     });
   });
 
@@ -299,195 +176,6 @@ describe("DataStore", () => {
       const json = store.toJSON("foo");
 
       expect(json.bar).to.equal(undefined);
-    });
-  });
-
-  describe("handlers", () => {
-    it("should ignore invalid handlers", () => {
-      store.useHandler();
-      store.useHandler(true);
-      store.useHandler(false);
-      store.useHandler(null);
-      store.useHandler(undefined);
-      store.useHandler("foo", true);
-      store.useHandler("foo", false);
-      store.useHandler("foo", null);
-      store.useHandler("foo", undefined);
-      expect(store._handlers).to.eql([]);
-    });
-    it("should allow middleware", () => {
-      let run = 0;
-
-      store.useHandler(context => {
-        run++;
-        expect(context.method).to.equal("reset");
-      });
-      store.reset({});
-      expect(run).to.equal(1);
-    });
-    it("should allow delegation", () => {
-      let run = 0;
-
-      store.useHandler(/zing/, context => {
-        run++;
-        context.value = "bar";
-        expect(context.key).to.equal("zing");
-        expect(context.method).to.equal("set");
-      });
-      store.set("zing", "foo");
-      expect(run).to.equal(1);
-      expect(store._data.zing).to.equal("bar");
-    });
-    it("should allow delegation when using setAll", () => {
-      let run = 0;
-
-      store.useHandler(/zing/, context => {
-        run++;
-        context.value = "bar";
-        expect(context.key).to.equal("zing");
-        expect(context.method).to.equal("set");
-      });
-      store.setAll({ zing: "foo", zang: "bar" });
-      expect(run).to.equal(1);
-      expect(store._data.zing).to.equal("bar");
-      expect(store._data.zang).to.equal("bar");
-    });
-    it("should allow handling with option merging", () => {
-      let run = 0;
-
-      store.useHandler(/foo/, context => {
-        run++;
-        context.merge("options", { merge: false });
-        expect(context.key).to.equal("foo");
-      });
-      store.set("foo", { bar: "bar" });
-      expect(store._data.foo).to.eql({ bar: "bar" });
-      expect(run).to.equal(1);
-    });
-    it("should allow multiple handlers", () => {
-      let run = 0;
-
-      store.useHandler(/zing/, context => {
-        run++;
-        context.value = "bar";
-      });
-      store.useHandler(/zing/, context => {
-        run++;
-      });
-      store.set("zing", "foo");
-      expect(store._data.zing).to.equal("bar");
-      expect(run).to.equal(2);
-    });
-    it("should remove a single handler", () => {
-      let run = 0;
-      const fn = context => {
-        run++;
-      };
-
-      store.useHandler(fn);
-      store.set("bar", "boo");
-      expect(run).to.equal(1);
-      store.unuseHandler(fn);
-      store.set("bar", "boop");
-      expect(run).to.equal(1);
-    });
-    it("should remove batched handlers", () => {
-      let run = 0;
-      const handlers = [
-        [
-          context => {
-            run++;
-          }
-        ],
-        [
-          context => {
-            run++;
-          }
-        ]
-      ];
-
-      store.useHandler(handlers);
-      store.set("bar", "boo");
-      expect(run).to.equal(2);
-      store.unuseHandler(handlers);
-      store.set("bar", "boop");
-      expect(run).to.equal(2);
-    });
-  });
-});
-
-describe("HandlerContext", () => {
-  describe("constructor", () => {
-    it("should assign passed args based on signature", () => {
-      const context = new HandlerContext(
-        {},
-        "set",
-        ["key", "value"],
-        ["foo", "bar"]
-      );
-
-      expect(context.key).to.equal("foo");
-      expect(context.value).to.equal("bar");
-    });
-    it("should assign passed args, including rest argument", () => {
-      const context = new HandlerContext(
-        {},
-        "set",
-        ["key", "value", "...args"],
-        ["foo", "bar", true]
-      );
-
-      expect(context.key).to.equal("foo");
-      expect(context.value).to.equal("bar");
-      expect(context.args).to.eql([true]);
-    });
-  });
-
-  describe("merge", () => {
-    it("should define missing options", () => {
-      const context = new HandlerContext(
-        {},
-        "set",
-        ["key", "value", "options"],
-        ["foo", "bar"]
-      );
-
-      context.merge("options", { merge: false });
-      expect(context.options).to.eql({ merge: false });
-    });
-    it("should merge existing options", () => {
-      const context = new HandlerContext(
-        {},
-        "set",
-        ["key", "value", "options"],
-        ["foo", "bar", { foo: true }]
-      );
-
-      context.merge("options", { merge: false });
-      expect(context.options).to.eql({ foo: true, merge: false });
-    });
-  });
-
-  describe("toArguments", () => {
-    it("should return args based on signature", () => {
-      const context = new HandlerContext(
-        {},
-        "set",
-        ["key", "value"],
-        ["foo", "bar"]
-      );
-
-      expect(context.toArguments()).to.eql(["foo", "bar"]);
-    });
-    it("should return args, including rest argument", () => {
-      const context = new HandlerContext(
-        {},
-        "set",
-        ["key", "value", "...args"],
-        ["foo", "bar", true]
-      );
-
-      expect(context.toArguments()).to.eql(["foo", "bar", true]);
     });
   });
 });
